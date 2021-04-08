f6f029c52ee280b2922706ed7aba88d74198de82
Aiana <mediaiana@mail.ru>
2021-04-08 21:24:45 +0300

    firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Form from './components/Form/Form';
 
 function App() {
   return (
     <div className="App">
       Here comes ya boi
+      <Form/>
     </div>
   );
 }
