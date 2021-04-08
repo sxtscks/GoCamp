de0f6ad862afb11d4d99ff51dd730913a3d42c3a
Nikolas <tesla.ff25@gmail.com>
2021-04-08 17:29:08 +0300

    start landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Landing from './components/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
-      Here comes ya boi
+      <Landing/>
     </div>
   );
 }
---
+++ client/src/components/Landing/Landing.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Landing.css'
+function Landing() {
+    return (
+        <div className='landing'>
+            <div className="container">
+                <div className="firstScreen">
+                    Hello
+                </div>
+            </div>
+            <div className="container">
+                <div className="firstScreen">
+                    Hello
+                </div>
+            </div>
+            <div className="container">
+                <div className="firstScreen">
+                    Hello
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Landing
