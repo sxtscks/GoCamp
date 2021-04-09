e6a4219a92d459f72d21887bc710a27138bc6b61
Kirill <cvj.sp.bk@gmail.com>
2021-04-09 17:51:02 +0300

    current trips page on redux

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import todosReducer from './todosReducer'
+import tripReducer from './tripReducer'
 import userReducer from './userReducer'
 
 const rootReducer = combineReducers({
   user: userReducer,
-  todos: todosReducer
+  todos: todosReducer,
+  trips: tripReducer,
 })
 
 export default rootReducer
